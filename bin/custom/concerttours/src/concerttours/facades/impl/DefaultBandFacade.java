c009f74f2c9308546f38d8f383d371de6ec9240c
Ignat Churilo <i.ignatch.com@mail.ru>
2022-01-25 01:49:27 +0300

    Add Media Files

--- bin/custom/concerttours/src/concerttours/facades/impl/DefaultBandFacade.java
+++ bin/custom/concerttours/src/concerttours/facades/impl/DefaultBandFacade.java
@@ -1,10 +1,13 @@
 package concerttours.facades.impl;
 
+import de.hybris.platform.core.model.media.MediaContainerModel;
+import de.hybris.platform.core.model.media.MediaFormatModel;
 import de.hybris.platform.core.model.product.ProductModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hybris.platform.servicelayer.media.MediaService;
 import org.springframework.beans.factory.annotation.Required;
 import concerttours.data.BandData;
 import concerttours.data.TourSummaryData;
@@ -17,17 +20,20 @@ import java.util.Locale;
 
 public class DefaultBandFacade implements BandFacade {
     private BandService bandService;
+    private MediaService mediaService;
 
     @Override
     public List<BandData> getBands() {
         final List<BandModel> bandModels = bandService.getBands();
         final List<BandData> bandFacadeData = new ArrayList<>();
+        final MediaFormatModel format = mediaService.getFormat("bandList");
         for (final BandModel sm : bandModels) {
             final BandData sfd = new BandData();
             sfd.setId(sm.getCode());
             sfd.setName(sm.getName());
             sfd.setDescription(sm.getHistory());
             sfd.setAlbumsSold(sm.getAlbumSales());
+            sfd.setImageURL(getImageURL(sm, format));
             bandFacadeData.add(sfd);
         }
         return bandFacadeData;
@@ -63,18 +69,33 @@ public class DefaultBandFacade implements BandFacade {
             }
         }
         // Now we can create the BandData transfer object
+        final MediaFormatModel format = mediaService.getFormat("bandDetail");
         final BandData bandData = new BandData();
         bandData.setId(band.getCode());
         bandData.setName(band.getName());
         bandData.setAlbumsSold(band.getAlbumSales());
-        bandData.setDescription(band.getHistory());
+        bandData.setImageURL(getImageURL(band, format));
+        bandData.setDescription(band.getHistory(Locale.ENGLISH));
         bandData.setGenres(genres);
         bandData.setTours(tourHistory);
         return bandData;
     }
 
+    protected String getImageURL(final BandModel sm, final MediaFormatModel format) {
+        final MediaContainerModel container = sm.getImage();
+        if (container != null) {
+            return mediaService.getMediaByFormat(container, format).getDownloadURL();
+        }
+        return null;
+    }
+
     @Required
     public void setBandService(final BandService bandService) {
         this.bandService = bandService;
+    }
+
+    @Required
+    public void setMediaService(MediaService mediaService) {
+        this.mediaService = mediaService;
     }
 }
--- bin/custom/concerttours/web/webroot/WEB-INF/views/BandList.jsp
+++ bin/custom/concerttours/web/webroot/WEB-INF/views/BandList.jsp
@@ -3,11 +3,11 @@
 <html>
 <title>Band List</title>
 <body>
-    <h1>Band List</h1>
-    <ul>
-        <c:forEach var="band" items="${bands}">
-            <li><a href="./bands/${band.id}">${band.name}</a></li>
-        </c:forEach>
-    </ul>
+<h1>Band List</h1>
+<ul>
+    <c:forEach var="band" items="${bands}">
+        <li><a href="./bands/${band.id}"><img src="${band.imageURL}" />${band.name}</a></li>
+    </c:forEach>
+</ul>
 </body>
 </html>
